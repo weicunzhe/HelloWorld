c87f98a52e78b4d4cf30256b5fdc58c43a561d7d
weicunzhe <weicunzhe@qq.com>
2024-09-22 00:03:35 +0800

    实现sort函数

--- main.cpp
+++ main.cpp
@@ -3,6 +3,23 @@ using namespace std;
 
 void sort(int arr[], int size)
 {
+    for(int i = 0; i < size; i++)
+    {
+        for(int j = i + 1; j < size; j++)
+        {
+            if(arr[i] > arr[j])
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    for(int i = 0; i < size; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
 }
 
 int main()
